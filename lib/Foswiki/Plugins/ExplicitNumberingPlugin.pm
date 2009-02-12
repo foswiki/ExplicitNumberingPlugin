c4e1daca8cb519cf36d49d55e394ab40980215c7
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-02-12 10:51:10 +0000

    Item1038:
       * code cleanup and regex fixes
       * added reset to specific number based on suggestion by TWiki:Main.SteveWampler
       * using preRenderingHanlder instead of commonTagsHandler to prevent numbers being
         expanded in textareas
    
    
    
    git-svn-id: http://svn.foswiki.org/trunk/ExplicitNumberingPlugin@2461 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ExplicitNumberingPlugin.pm
+++ lib/Foswiki/Plugins/ExplicitNumberingPlugin.pm
@@ -11,20 +11,20 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at
+# GNU General Public License for more details, published at 
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
 #
 
 # =========================
-package Foswiki::Plugins::ExplicitNumberingPlugin;
+package Foswiki::Plugins::ExplicitNumberingPlugin; 
 
 # =========================
 use vars qw(
-  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-  $debug
-);
+        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+        $debug
+    );
 
 # This should always be $Rev$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -36,28 +36,27 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Foswiki';
 
-$pluginName = 'ExplicitNumberingPlugin';    # Name of this Plugin
+$pluginName = 'ExplicitNumberingPlugin';  # Name of this Plugin
 
-my $maxLevels = 6;    # Maximum number of levels
-my %Sequences;        # Numberings, addressed by the numbering name
-my $lastLevel = $maxLevels - 1;    # Makes the code more readable
-my @alphabet  = (
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-);
+
+my $maxLevels = 6;              # Maximum number of levels
+my %Sequences;                  # Numberings, addressed by the numbering name
+my $lastLevel = $maxLevels - 1; # Makes the code more readable
+my @alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 
 # =========================
-sub initPlugin {
+sub initPlugin
+{
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if ( $Foswiki::Plugins::VERSION < 1 ) {
-        Foswiki::Func::writeWarning(
-            "Version mismatch between $pluginName and Plugins.pm");
+    if( $Foswiki::Plugins::VERSION < 1 ) {
+        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
         return 0;
     }
 
-    $debug = Foswiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
+
+    $debug = Foswiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
 
     # Plugin correctly initialized
     ##Foswiki::Func::writeDebug( "- Foswiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
@@ -70,96 +69,89 @@ sub initPlugin {
 # auto-numbering of heading levels, otherwise the TOC lines will have
 # different number than the heading line (must be done before TOC).
 
-sub commonTagsHandler {
+sub preRenderingHandler {
 ### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
 
     ##Foswiki::Func::writeDebug( "- ${pluginName}::commonTagsHandler( $web.$topic )" ) if $debug;
 
-    return if $_[3];    # Called in an include; do not number yet.
-
     %Sequences = ();
 
-    $_[0] =~ s/\-\-\-(\#\#*) /&makeHeading(length($1))/geo;
-    $_[0] =~
-s/\#\#(\w+\#)?([0-9]+)?\.(\.*)([a-z]?)/&makeExplicitNumber($1,$2,length($3),$4)/geo;
+    $_[0] =~ s/(^---+\+*)(\#+)([0-9]*)/$1.&makeHeading(length($2), $3)/gem;
+    $_[0] =~ s/\#\#(\w+\#)?([0-9]+)?\.(\.*)([a-zA-Z]?)/&makeExplicitNumber($1,$2,length($3),$4)/ge;
 }
 
 # =========================
 
 sub makeHeading {
-    my $headerlvl = shift || 0;
-    my $headerlevel = ($headerlvl) ? '---' : '';
-    my $numlevel = '##';
-    for ( my $i = 0 ; $i < $headerlvl ; $i++ ) {
-        $headerlevel .= '+';
-        $numlevel    .= '.';
+    my ($level, $init) = @_;
+
+
+    $init = '' unless defined $init;
+
+    my $result = '';
+    my $numlevel = '';
+    for (my $i = 0; $i < $level; $i++) {
+      $result .= '+';
+      $numlevel .= '.';
     }
-    return $headerlevel . $numlevel . ' ';
+
+    return $result.'##'.$init.$numlevel.' ';
 }
 
 # Build the explicit outline number
-sub makeExplicitNumber {
+sub makeExplicitNumber
+{
+    my ($name, $init, $level, $alist) = @_;
 
     ##Foswiki::Func::writeDebug( "- ${pluginName}::makeExplicitNumber( $_[0], $_[1], $_[2], $_[3] )" ) if $debug;
 
-    my $name  = '-default-';
-    my $init  = '';
-    my $level = $_[2];
-    my $alist = '';
-    $name  = $_[0] if defined $_[0];
-    $init  = $_[1] if defined $_[1];
-    $alist = $_[3] if defined $_[3];
-    if ( $alist ne '' ) {
-        $level++;
-    }
+    $name = '-default-' unless defined $name;
+    $alist = '' unless defined $alist;
+    $level++ if $alist ne '';
 
-    my $text = '';
 
     #...Truncate the level count to maximum allowed
-    if ( $level > $lastLevel ) { $level = $lastLevel; }
+    if ($level > $lastLevel) { $level = $lastLevel; }
 
     #...Initialize a new, or get the current, numbering from the Sequences
     my @Numbering = ();
-    if ( !defined( $Sequences{$name} ) ) {
+    if ( ! defined( $Sequences{$name} ) ) {
         for $i ( 0 .. $lastLevel ) { $Numbering[$i] = 0; }
-    }
-    else {
-        @Numbering = split( ':', $Sequences{$name} );
-
+    } else {
+        @Numbering = @{$Sequences{$name}};
         #...Re-initialize the sequence
-        if ( defined $_[1] ) {
-            $init = ( int $init );
-            if ($init) {
-                $Numbering[$level] = $init - 1;
-            }
-            else {
-                for $i ( 0 .. $lastLevel ) { $Numbering[$i] = 0; }
-            }
-        }
+    }
+
+    if ( defined $init ) {
+      $init = (int $init);
+      $Numbering[$level] = $init - 1;
     }
 
     #...Increase current level number
-    $Numbering[$level] += 1;
+    $Numbering[ $level ] += 1;
 
     #...Reset all higher level counts
     if ( $level < $lastLevel ) {
-        for $i ( ( $level + 1 ) .. $lastLevel ) { $Numbering[$i] = 0; }
+        for $i ( ($level+1) .. $lastLevel ) { $Numbering[$i] = 0; }
     }
 
     #...Save the altered numbering
-    $Sequences{$name} = join( ':', @Numbering );
+    $Sequences{$name} = \@Numbering;
 
     #...Construct the number
+    my $text = '';
     if ( $alist eq '' ) {
         for $i ( 0 .. $level ) {
             $text .= "$Numbering[$i]";
             $text .= '.' if ( $i < $level );
         }
-    }
-    else {
-
+    } else {
         #...Level is 1-origin, indexing is 0-origin
-        $text .= $alphabet[ $Numbering[$level] - 1 ];
+        if ($alist =~ /[A-Z]/){
+            $text .= uc $alphabet[$Numbering[$level]-1];
+        } else {
+            $text .= $alphabet[$Numbering[$level]-1];
+        }
     }
 
     return $text;
