7899e13b390ccb125c7ed69ccca0e432505a869c
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:52:31 +0000

    Item8765: Replace use base with use; our @ISA; pass 2
    git-svn-id: http://svn.foswiki.org/trunk/ExplicitNumberingPlugin@6913 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/ExplicitNumberingPlugin/ExplicitNumberingPluginSuite.pm
+++ test/unit/ExplicitNumberingPlugin/ExplicitNumberingPluginSuite.pm
@@ -1,5 +1,6 @@
 package ExplicitNumberingPluginSuite;
-use base 'Unit::TestSuite';
+use Unit::TestSuite;
+our @ISA = qw( Unit::TestSuite );
 
 sub include_tests { return 'ExplicitNumberingPluginTests' }
 
