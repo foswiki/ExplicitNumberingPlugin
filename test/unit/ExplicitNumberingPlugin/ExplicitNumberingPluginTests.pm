f78a29e73aa44c5d10dbf5ca3cd594f455b57f00
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:51:12 +0000

    Item8765: Replace use base with use; our @ISA;
    git-svn-id: http://svn.foswiki.org/trunk/ExplicitNumberingPlugin@6912 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/ExplicitNumberingPlugin/ExplicitNumberingPluginTests.pm
+++ test/unit/ExplicitNumberingPlugin/ExplicitNumberingPluginTests.pm
@@ -2,7 +2,8 @@ use strict;
 
 package ExplicitNumberingPluginTests;
 
-use base qw( FoswikiFnTestCase );
+use FoswikiFnTestCase;
+our @ISA = qw( FoswikiFnTestCase );
 
 #use base qw(FoswikiTestCase);
 
